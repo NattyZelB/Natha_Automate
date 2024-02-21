7a821f21780cabf2d69b7ec8693c9f7d781b5399
NattyZelB <92927774+NattyZelB@users.noreply.github.com>
2024-02-21 11:21:26 +0100

    home base authentication

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dataentry',
     'uploads',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -152,4 +154,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 DEFAULT_TO_EMAIL = 'nattananphermsin@gmail.com'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
